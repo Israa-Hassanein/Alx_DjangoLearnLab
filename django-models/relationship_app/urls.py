66ecea6b06fc918dda5f939a781733bec0fdffe0
israa-hassanein <israa.hassanein@gmail.com>
2024-11-10 23:59:19 +0200

    task 2

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),           # Function-based view for books
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library
+]
+
+
+urlpatterns = [
+    path('relationship_app/', include('relationship_app.urls')),  # Include the app's URLs
+]
