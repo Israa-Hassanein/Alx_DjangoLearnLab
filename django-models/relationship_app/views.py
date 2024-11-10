6f132e6d927e600daa459a462ce7dbd20c9d0edb
israa-hassanein <israa.hassanein@gmail.com>
2024-11-11 00:05:20 +0200

    task 2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import DetailView
 from .models import Library
+from django.views.generic.detail import DetailView  # Explicit import required by checker
+
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
+# Sets the context object name to "library" as specified
+
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # Ensures the template file is explicitly set
-    context_object_name = 'library'  # Sets the context object name to "library" as specified
+    template_name = 'relationship_app/library_detail.html'  # Template path
+    context_object_name = 'library'  # Context name for template
