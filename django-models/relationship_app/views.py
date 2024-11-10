0b878798e4d18afb1c4b4975303752f54747ca18
israa-hassanein <israa.hassanein@gmail.com>
2024-11-11 00:02:36 +0200

    changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,3 +6,9 @@ from .models import Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Ensures the template file is explicitly set
+    context_object_name = 'library'  # Sets the context object name to "library" as specified
