79b62322f4779cdb153340c60d3ebd70b27e2422
israa-hassanein <israa.hassanein@gmail.com>
2024-11-16 23:35:00 +0200

    latest changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -41,11 +41,12 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-]
-
-
-urlpatterns = [
-    path('add/', views.add_book, name='add_book'),
+        path('add/', views.add_book, name='add_book'),
     path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('admin/', admin.site.urls),
+    path('books/', include('relationship_app.urls')),  # Make sure this line exists
 ]
+
+
+
