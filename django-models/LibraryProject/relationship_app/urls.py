37d8f8b2979dc4b116d48e38ddcd31086ce3b6de
israa-hassanein <israa.hassanein@gmail.com>
2024-11-16 23:19:11 +0200

    16-11 changes 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
@@ -37,6 +38,9 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     # Add more URL patterns as needed
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
 
 
