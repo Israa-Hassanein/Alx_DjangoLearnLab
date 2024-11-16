a7c2a3d9970f752708611f131c4feb79b3208e70
israa-hassanein <israa.hassanein@gmail.com>
2024-11-16 21:02:14 +0200

    changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from .models import Book
-from django.views.generic import DetailView
-from .models import Library
-from django.views.generic.detail import DetailView  # Explicit import required by checker
+from .models import Book, Library
+from django.views.generic.detail import DetailView
 
 
 def list_books(request):
@@ -11,8 +9,14 @@ def list_books(request):
 
 
 # Sets the context object name to "library" as specified
-
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # Template path
     context_object_name = 'library'  # Context name for template
+
+    def get_context_data(self, **kwargs):
+        # Get the default context data
+        context = super().get_context_data(**kwargs)
+        # Add the books related to the specific library
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
