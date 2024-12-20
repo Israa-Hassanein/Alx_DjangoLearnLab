10c9ae888b902f9f0f7a1850a703680dce2e451e
israa-hassanein <israa.hassanein@gmail.com>
2024-12-20 21:57:28 +0200

    social media app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,17 +1,42 @@
-from rest_framework import serializers
-from .models import CustomUser
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
+    # Fields for the user model
+    bio = serializers.CharField(required=False, allow_blank=True)
+    profile_picture = serializers.CharField(required=False, allow_blank=True)  # Use ImageField if you store actual images
+    followers = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all(), many=True)
+
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        model = get_user_model()  # Use custom user model
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # Custom registration serializer
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password', 'password2')
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        # Create the user and return the user object
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)  # Create a token when user is created
         return user
 
-
-    
+class LoginSerializer(serializers.Serializer):
+    # Serializer for user login (authentication)
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from .views import RegisterView
 from django.contrib import admin
 from django.urls import include
+from . import views
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('admin/', admin.site.urls),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
