862f3270803063d081d0b45c7279de970ec13366
israa-hassanein <israa.hassanein@gmail.com>
2024-12-20 22:00:21 +0200

    social media app

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 from django.urls import include
 from . import views
 
-
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('admin/', admin.site.urls),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
